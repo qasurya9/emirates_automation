31b6caa1601e1c220aa6dc834e6e7a5a22156938
qasurya9 <45616800+qasurya9@users.noreply.github.com>
2018-12-05 14:43:30 +0530

    added comments for understanding POM JS classes

--- src/pages/emirates_home_page.js
+++ src/pages/emirates_home_page.js
@@ -4,6 +4,9 @@ var EmHomePage = function() {
     this.departure_text_field     = element(by.name("Departure airport"));
     this.arrival_text_field       = element(by.name("Arrival airport"));
     this.departing_date           = element(by.xpath(""));
+    
+    // Also add required methods to perform opeations on this page
+    // All these methods can be called in required step definitions based on requirement.
 
 };
 
